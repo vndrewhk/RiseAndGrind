47c135e6f8042e3779cc65ac4e0ad5d3b9f6a098
Andrew Ma <andrewhkma@gmail.com>
2023-07-18 22:20:58 -0400

    added transition on logo hover

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className="outline-1 outline-black outline min-h-[10vh] p-2 sm:p-1  lg:p-3 max-h-[10vh] flex flex-row justify-between">
       <a className="min-h-[100%] flex">
-        <img src="/logo.png" className=" object-contain"></img>
+        <img src="/logo.png" className="cursor-pointer object-contain transition hover:scale-110"></img>
       </a>
       <div className="flex items-center lg:gap-x-4">
         <Link href="/" className="invisible lg:visible">LeetCode</Link>
