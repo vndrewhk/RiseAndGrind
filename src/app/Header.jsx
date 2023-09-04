eb4d23c22cb907b5a5d09b645bb0dbe1c3e63c18
Andrew Ma <drewhkm@gmail.com>
2023-09-03 21:48:14 -0400

    changed header signin, added debugging for session

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -4,11 +4,12 @@ import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
 import AuthModal from "@/components/AuthModal";
-
+import { useSession } from "next-auth/react";
 
 function Header() {
   let [authModal, setAuthModal] = useState(false);
-
+  const { data: session, status } = useSession();
+  console.log(session)
   return (
     <div className="outline-1 outline-black outline min-h-[10vh] p-2 sm:p-1  lg:p-3 max-h-[10vh] flex flex-row justify-between">
       <Link href="/" className="min-h-[100%] flex select-none">
@@ -32,19 +33,28 @@ function Header() {
           Career
         </Link>
         {/* <Link href="/" className="invisible lg:visible">Resources</Link> */}
-        <Link
-          href="/"
-          className="invisible lg:visible   hover:outline outline-2 outline-black p-2"
-          onClick={() => setAuthModal(!authModal)}
-        >
-          Log in
-        </Link>
+        {status === "authenticated" ? (
+          <>
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        ) : (
+          <Link
+            href="/"
+            className="invisible lg:visible   hover:outline outline-2 outline-black p-2"
+            onClick={() => setAuthModal(!authModal)}
+          >
+            Log in
+          </Link>
+        )}
         <Link href="/" className="visible lg:invisible">
           <MenuIcon />
         </Link>
       </div>
       {authModal && (
-        <AuthModal authModal={authModal} setAuthModal={setAuthModal}></AuthModal>
+        <AuthModal
+          authModal={authModal}
+          setAuthModal={setAuthModal}
+        ></AuthModal>
       )}
     </div>
   );
